41cdbc9486893e842f80cad031b96b49a251259d
ff3f1 <ff3f1@yandex.ru>
2025-04-08 23:45:14 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,5 @@
 import { TelegramClient } from "telegram";
-import { StringSession } from "telegram/sessions";
+import { StringSession } from "telegram/sessions/index.js";
 import input from "input";
 import fs from "fs";
 import dotenv from "dotenv";
