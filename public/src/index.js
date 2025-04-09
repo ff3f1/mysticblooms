07992bebe30ebf8633a273a14b96e94cff67441e
ff3f1 <ff3f1@yandex.ru>
2025-04-09 13:33:48 +0300

    Update index.js

--- public/src/index.js
+++ public/src/index.js
@@ -1,20 +1,3 @@
-fetch("/posts.json")
-  .then((res) => res.json())
-  .then((posts) => {
-    const container = document.getElementById("posts");
-
-    posts.forEach((post) => {
-      const div = document.createElement("div");
-      div.className = "mb-4 p-4 bg-white rounded-xl shadow";
-
-      div.innerHTML = `
-        <p class="text-gray-800 whitespace-pre-line">${post.text}</p>
-        <p class="text-xs text-gray-400 mt-2">${new Date(post.date).toLocaleString()}</p>
-      `;
-
-      container.appendChild(div);
-    });
-  });
 // public/src/index.js
 async function loadPosts() {
   const response = await fetch('/posts.json');
